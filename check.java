a191a890a5b0ba61e0e3f836ddab4958036ca2a8
Owusu56 <165980842+Owusu56@users.noreply.github.com>
2025-02-04 14:39:06 -0800

    commit

--- check.java
+++ check.java
@@ -3,21 +3,39 @@ public  class check{
 
 public static void main(String[] args) {
 
-    for (int i = 0; i < main().length; i++) {
-        System.out.println(main()[i]);
+    
+        // System.out.println(main()[i]);
         
-    }
+        System.out.println(BigNum(109.4, 207.5, 300.6));
     
 }
 
 
-public static String[] main() {
+// public static String[] main() {
 
- String Engineers[] = {"Jones", "Giscard", "Kay"};
+//  String Engineers[] = {"Jones", "Giscard", "Kay"};
 
- return  Engineers;
+//  return  Engineers;
 
 
+// }
+
+
+public static double BigNum (double num1, double  num2, double  num3) {
+    double BigNum = 0.0;
+
+    // num1 = 10.4;
+    // num2 = 20.5;
+    // num3 = 30.6;
+    
+    if (num1 > num2 && num1 > num3) {
+        BigNum = num1;
+    } else if (num2 > num1 && num2 > num3) {
+        BigNum = num2;
+    } else {
+        BigNum = num3;
+    }
+    return BigNum;
 }
 
 
